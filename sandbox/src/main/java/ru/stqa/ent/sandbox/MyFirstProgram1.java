982de90b1bda919dc3f4a5dc39ca559a067e42fd
akschva <Whiterabbit02@mail.ru>
2025-02-04 16:06:42 +0300

    Update MyFirstProgram1.java

--- sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
@@ -1,13 +1,23 @@
 package ru.stqa.ent.sandbox;
 
+import javax.xml.transform.Source;
+import java.sql.SQLOutput;
+
 public class MyFirstProgram1 {
 
   public static void main(String[] args) {
     meow("kitty");
+    meow("birdy");
+
+    double l = 5;
+    System.out.println("Square area " + l + " = " + area(l));
   }
 
   public static void meow(String something) {
     System.out.println("Meow "+ something + "!");
   }
 
+  public static double area(double len) {
+    return len * len;
+  }
 }
