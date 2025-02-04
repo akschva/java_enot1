e62e8fceb72b5efd91292ef2e76fc2730f0a7530
akschva <Whiterabbit02@mail.ru>
2025-02-04 14:14:06 +0300

    Update MyFirstProgram1.java

--- sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
@@ -4,6 +4,12 @@ public class MyFirstProgram1 {
 
   public static void main(String[] args) {
     System.out.println("Meow meow!");
+
+    System.out.println(2 + 2.0);
+
+    System.out.println("m" + "e" + "o" + "w" + " :3");
+
+    System.out.println("3 + 3 = " + (3 * 3));
   }
 
 }
