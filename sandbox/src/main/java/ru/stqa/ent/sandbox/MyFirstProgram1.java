6594c9b9b6e6279b9d74af8d4f1bee716ae96156
akschva <Whiterabbit02@mail.ru>
2025-02-04 15:48:29 +0300

    Update MyFirstProgram1.java

--- sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
@@ -3,18 +3,11 @@ package ru.stqa.ent.sandbox;
 public class MyFirstProgram1 {
 
   public static void main(String[] args) {
-    String something = "meowww";
+    meow("kitty");
+  }
+
+  public static void meow(String something) {
     System.out.println("Meow "+ something + "!");
-
-    System.out.println(2 + 2.0);
-
-    System.out.println("m" + "e" + "o" + "w" + " :3");
-
-    System.out.println("3 + 3 < " + (3 * 3));
-
-    double l = 6;
-    double s = l * l;
-    System.out.println("Square area with side " + l + " = " + s);
   }
 
 }
