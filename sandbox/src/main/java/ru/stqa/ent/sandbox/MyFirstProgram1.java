7d2bd3db379b1317e6ea8227008e2761da424796
akschva <Whiterabbit02@mail.ru>
2025-02-04 14:01:24 +0300

    Изменено форматирование

--- sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
@@ -3,7 +3,7 @@ package ru.stqa.ent.sandbox;
 public class MyFirstProgram1 {
 
   public static void main(String[] args) {
-   System.out.println("Meow meow!"); 
- }
- 
+    System.out.println("Meow meow!");
+  }
+
 }
