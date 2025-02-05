7b64ce6a87169848684a03f54a6bd9d78df3c1c4
akschva <Whiterabbit02@mail.ru>
2025-02-05 17:01:56 +0300

    Исправили орфографические ошибки и опечатки в комментах

--- sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
@@ -12,9 +12,9 @@ public class MyFirstProgram1 {
     Square s = new Square(7);
     System.out.println("Square area " + s.l + " = " + area(s));
     /* когда мы передавали не простое число double l = 5, а объект типа Square,
-    * мы создавали Square s = new Square(); и установили передаваемое значение атрибута s.l = 7
-    * и уже объект типа Square становился параметром функции area(s), а не атрибут длина стороны квадрата area(l)
-    * Было Square s = new Square(); s.l (из класса) = 7 и area(s) */
+     * мы создавали Square s = new Square(); и установили передаваемое значение атрибута s.l = 7
+     * и уже объект типа Square становился параметром функции area(s), а не атрибут длина стороны квадрата area(l)
+     * Было Square s = new Square(); s.l (из класса) = 7 и area(s) */
 
 
     Rectangle m = new Rectangle(7, 4);
@@ -43,8 +43,8 @@ public class MyFirstProgram1 {
 
   public static double area(Square s) {
     return s.l * s.l;
-    /* для того, чтобы получить доступ к атрибуту объекта, мы пишем s.l
-    * И сейчас в фунцкию area передается объект типа квадрат, а не длина стороны квадрата атрибут (double l)*/
+    /* для того чтобы получить доступ к атрибуту объекта, мы пишем s.l
+     * И сейчас в функцию area передается объект типа квадрат, а не длина стороны квадрата атрибут (double l)*/
   }
 
   public static double area(Rectangle m) {
