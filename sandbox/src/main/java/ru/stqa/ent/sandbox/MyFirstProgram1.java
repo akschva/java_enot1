db3646ac5a552d24ba2f125541b37f6a823d6355
akschva <Whiterabbit02@mail.ru>
2025-02-05 10:49:03 +0300

    Описание внесенных после создания классов (и конструкторов в них) изменений

--- sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
+++ sandbox/src/main/java/ru/stqa/ent/sandbox/MyFirstProgram1.java
@@ -9,15 +9,28 @@ public class MyFirstProgram1 {
     meow("kitty");
     meow("birdy");
 
-    double l = 5;
-    System.out.println("Square area " + l + " = " + area(l));
+    Square s = new Square(7);
+    System.out.println("Square area " + s.l + " = " + area(s));
+    /* когда мы передавали не простое число double l = 5, а объект типа Square,
+    * мы создавали Square s = new Square(); и установили передаваемое значение атрибута s.l = 5
+    * и уже объект типа Square становился параметром функции area(s), а не атрибут длина стороны квадрата area(l)*/
+
+
+    Rectangle m = new Rectangle(7, 4);
+    System.out.println("Rectangle area " + m.a + " and " + m.b + " = " + area(m));
   }
 
   public static void meow(String something) {
     System.out.println("Meow "+ something + "!");
   }
 
-  public static double area(double len) {
-    return len * len;
+  public static double area(Square s) {
+    return s.l * s.l;
+    /* для того, чтобы получить доступ к атрибуту объекта, мы пишем s.l
+    * И сейчас в фунцкию area передается объект типа квадрат, а не длина стороны квадрата атрибут (double l)*/
+  }
+
+  public static double area(Rectangle m) {
+    return m.a * m.b;
   }
 }
