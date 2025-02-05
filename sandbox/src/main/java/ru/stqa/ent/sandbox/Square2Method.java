bb4d74b5558ab7f28bf9d0ca8ccfb804d661c83a
akschva <Whiterabbit02@mail.ru>
2025-02-05 16:24:06 +0300

    Дополнительно прокомментировала отличие функции и метода

--- sandbox/src/main/java/ru/stqa/ent/sandbox/Square2Method.java
+++ sandbox/src/main/java/ru/stqa/ent/sandbox/Square2Method.java
@@ -10,9 +10,9 @@ public class Square2Method {
   public double area() {
     return this.d * this.d;
 
- /* было public static double area(Square2Method n) {return n.d * n.d;}
- * убираем static и параметр т.к. метод ассоциирован с объектом, его не нужно передавать
- * обращаться к ассоциированному объекту можно тоже через this*/
+ /* Было public static double area(Square2Method n) {return n.d * n.d;}
+ * убираем static (оно для функций неассоциированных ни с каким объектом, к ним можно обращаться напрямую) и параметр т.к. метод ассоциирован с объектом, его не нужно передавать
+ * обращаться к ассоциированному объекту можно тоже через this */
 
   }
 
