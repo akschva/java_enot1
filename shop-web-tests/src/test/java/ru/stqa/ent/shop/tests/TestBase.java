8114e9db19db7d026a89967bbc43ff8dfe1fc5dd
akschva <Whiterabbit02@mail.ru>
2025-03-24 14:51:02 +0300

    Приколы с локальным методом

--- shop-web-tests/src/test/java/ru/stqa/ent/shop/tests/TestBase.java
+++ shop-web-tests/src/test/java/ru/stqa/ent/shop/tests/TestBase.java
@@ -1,7 +1,15 @@
 package ru.stqa.ent.shop.tests;
 
 import org.junit.After;
+import org.junit.AfterClass;
 import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.platform.suite.api.AfterSuite;
+import org.junit.platform.suite.api.BeforeSuite;
+import org.junit.runners.Parameterized;
+import org.openqa.selenium.By;
 import ru.stqa.ent.shop.appmanager.ApplicationManager;
 
 public class TestBase {
@@ -18,6 +26,17 @@ public class TestBase {
   @After
   public void tearDown() {
     app.stop();
+  }
+
+  @Parameterized.BeforeParam
+  public void clearcartPreconditions () {
+    if (app.getCartHelper().isThereAnItem()) {
+      do {
+        app.getClickHelper().click(By.xpath("/html/body/div[4]/div[1]/div[4]/div/div/div[2]/div/form/table/tbody/tr[1]/td[1]/input"));
+        app.getClickHelper().click(By.name("updatecart"));
+      }
+      while (app.getCartHelper().isThereAnItem());
+    }
   }
 
 }
