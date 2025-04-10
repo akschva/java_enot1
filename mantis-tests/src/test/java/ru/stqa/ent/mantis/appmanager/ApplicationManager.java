ee85d2f3c1570289286d0887fbd4d29b7e5d356c
akschva <Whiterabbit02@mail.ru>
2025-04-10 11:18:13 +0300

    Изменение структуры менеджера, чтобы браузер без необходимости не запускался

--- mantis-tests/src/test/java/ru/stqa/ent/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/ent/mantis/appmanager/ApplicationManager.java
@@ -17,9 +17,10 @@ import java.util.concurrent.TimeUnit;
 // класс для вспомогательных функций
 public class ApplicationManager {
   private final Properties properties;
-  public WebDriver driver;
+  private WebDriver driver;
 
   private String browser;
+  private RegistrationHelper registrationHelper;
 
   public ApplicationManager(String browser) {
     this.browser = browser;
@@ -34,43 +35,58 @@ public class ApplicationManager {
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
     properties.load(new FileReader(new File(String.format("src/test/resources/$s.properties",target))));
-
-//    String browser = "firefox";
-//Checking for 'firefox' parameters
-    if(browser.equalsIgnoreCase("firefox")){
-//creating firefox instance
-      System.setProperty("webdriver.firefox.bin","C:\\Program Files\\Mozilla Firefox\\firefox.exe");
-      driver = new FirefoxDriver();
-    }
-//Checking for 'chrome' parameters
-    else if(browser.equalsIgnoreCase("chrome")){
-//Creating chrome instance
-      driver = new ChromeDriver();
-      ChromeOptions options = new ChromeOptions();
-      options.setAcceptInsecureCerts(true);
-    }
-    else{
-//If the browser is not passed then throw the below exception
-      try {
-        throw new Exception("Invalid browser");
-      } catch (Exception e) {
-        throw new RuntimeException(e);
-      }
-    }
-    driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
-    driver.get(properties.getProperty("web.baseUrl"));
   }
 
 
   public void stop() {
-    driver.quit();
+    if (driver != null) {
+      driver.quit();
+    }
   }
 
   public  HttpSession newSession() {
-    return new HttpSession(this);
+    return new HttpSession(this); /* this это ссылка на аппменеджер*/
   }
 
   public String getProperty(String key) {
     return properties.getProperty(key);
+  }
+
+  public RegistrationHelper registration() {
+    if (registrationHelper == null) {
+      registrationHelper = new RegistrationHelper(this);
+    }
+    return registrationHelper;
+  }
+
+  /* метод инициализирует драйвер при первом обращении*/
+  public WebDriver getDriver() {
+    if (driver == null) {
+      //    String browser = "firefox";
+//Checking for 'firefox' parameters
+      if(browser.equalsIgnoreCase("firefox")){
+//creating firefox instance
+        System.setProperty("webdriver.firefox.bin","C:\\Program Files\\Mozilla Firefox\\firefox.exe");
+        driver = new FirefoxDriver();
+      }
+//Checking for 'chrome' parameters
+      else if(browser.equalsIgnoreCase("chrome")){
+//Creating chrome instance
+        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.setAcceptInsecureCerts(true);
+      }
+      else{
+//If the browser is not passed then throw the below exception
+        try {
+          throw new Exception("Invalid browser");
+        } catch (Exception e) {
+          throw new RuntimeException(e);
+        }
+      }
+      driver.manage().timeouts().implicitlyWait(0, TimeUnit.SECONDS);
+      driver.get(properties.getProperty("web.baseUrl"));
+    }
+    return driver;
   }
 }
