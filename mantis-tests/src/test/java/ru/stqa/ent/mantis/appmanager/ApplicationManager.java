cdc23112de0858feb5fb1db79a297daab9888b49
akschva <Whiterabbit02@mail.ru>
2025-04-10 13:31:55 +0300

    Ссылка исправлена на абсолютную

--- mantis-tests/src/test/java/ru/stqa/ent/mantis/appmanager/ApplicationManager.java
+++ mantis-tests/src/test/java/ru/stqa/ent/mantis/appmanager/ApplicationManager.java
@@ -34,7 +34,7 @@ public class ApplicationManager {
 
   public void init() throws IOException {
     String target = System.getProperty("target", "local");
-    properties.load(new FileReader(new File(String.format("src/test/resources/%s.properties",target))));
+    properties.load(new FileReader(new File(String.format("C:\\Users\\Admin\\Documents\\GitHub\\java_enot1\\mantis-tests\\src\\test\\resources\\local.properties", target))));
   }
 
 
